2ab615eeaac4b8692aabdf92e3fddcb00aa99afb
wai-yanphyo <waiyanphyo830@gmail.com>
2025-06-25 02:20:46 +0100

    Adding Header Component and Drawer COmponent in Template.jsx

---
+++ Frontend/src/Template.jsx
@@ -0,0 +1,20 @@
+import { Box, Container } from "@mui/material";
+
+
+ import Header from "./components/Header";
+ import AppDrawer from "./components/AppDrawer";
+
+ export default function Template() {
+
+    return (
+    <Box>
+        
+      <Header />
+      <AppDrawer />
+
+      
+      
+      
+    </Box>
+  );
+ }
