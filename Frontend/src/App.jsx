b89c417b13314b1e6eb629d76b0d82ed3b5af65b
wai-yanphyo <waiyanphyo830@gmail.com>
2025-06-25 01:14:12 +0100

    Testing the Header Component ny importing  in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Header from './components/Header';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>Hello</div>
+   <Header />
+     
   )
 }
 
