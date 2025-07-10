babd413daa6e1baa55b740fa3aaa57707855e911
wai-yanphyo <waiyanphyo830@gmail.com>
2025-07-10 21:01:19 +0100

    Ceate new file userController and adding getallusers

---
+++ Backend/controllers/userControllers.js
@@ -0,0 +1,21 @@
+const prisma = require('../config/db');
+
+const { Role } = require('@prisma/client');
+
+
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await prisma.user.findMany({
+            select: {
+                id: true,
+                email: true,
+                role: true,
+                createdAt: true,
+            }
+        });
+        res.status(200).json(users);
+    } catch (error) {
+        console.error('Error fetching all users:', error);
+        res.status(500).json({ message: 'Server error while fetching users.' });
+    }
+};
