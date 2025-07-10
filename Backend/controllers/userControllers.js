bfec87bfc08550146e33af4b6b3d170d29e2ed22
wai-yanphyo <waiyanphyo830@gmail.com>
2025-07-10 21:02:26 +0100

    adding updateUserRole in UserController

--- Backend/controllers/userControllers.js
+++ Backend/controllers/userControllers.js
@@ -19,3 +19,33 @@ const getAllUsers = async (req, res) => {
         res.status(500).json({ message: 'Server error while fetching users.' });
     }
 };
+
+
+
+const updateUserRole = async (req, res) => {
+    const userIdToUpdate = parseInt(req.params.id);
+    const { role } = req.body;
+
+     
+    if (isNaN(userIdToUpdate) || !role || !Object.values(Role).includes(role)) {
+        return res.status(400).json({ message: 'Invalid user ID or role provided.' });
+    }
+    
+    try {
+        const updatedUser = await prisma.user.update({
+            where: { id: userIdToUpdate },
+            data: { role: role},
+            select: { id: true, email: true, role: true } // Return updated user info
+        });
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.error('Error updating user role:', error);
+        res.status(500).json({ message: 'Server error while updating user role.' });
+    }
+};
+
+module.exports = {
+    getAllUsers,
+    updateUserRole,
+};
+
